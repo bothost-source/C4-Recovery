58edf8cf7087a9c217d6e293282520c97ead9285
LONER <destinyjob493@gmail.com>
2026-09-03 11:08:39 +0100

    Update start.js

--- handlers/start.js
+++ handlers/start.js
@@ -1,62 +1,82 @@
 const { getLocale, isOwner } = require('../utils/helpers');
 const { quote } = require('../utils/formatters');
-const { langKeyboard } = require('../utils/keyboards');
+const { langKeyboard, mainMenuKeyboard } = require('../utils/keyboards');
 const db = require('../database/db');
 const config = require('../config');
 
 async function handleStart(ctx) {
-    const userId = ctx.from.id;
-    const username = ctx.from.username || '';
-    const firstName = ctx.from.first_name || '';
-    const lastName = ctx.from.last_name || '';
+    try {
+        const userId = ctx.from.id;
+        const username = ctx.from.username || '';
+        const firstName = ctx.from.first_name || '';
+        const lastName = ctx.from.last_name || '';
 
-    await db.addUser(userId, username, firstName, lastName);
+        await db.addUser(userId, username, firstName, lastName);
 
-    const locale = getLocale(config.DEFAULT_LANG);
+        const locale = getLocale(config.DEFAULT_LANG);
 
-    const caption = quote(
-        locale.WELCOME_TITLE + '\n\n' +
-        locale.WELCOME_DESC + '\n\n' +
-        locale.WELCOME_FOOTER
-    );
+        const caption = quote(
+            locale.WELCOME_TITLE + '\n\n' +
+            locale.WELCOME_DESC + '\n\n' +
+            locale.WELCOME_FOOTER
+        );
 
-    if (config.WELCOME_IMAGE_URL) {
-        await ctx.replyWithPhoto(config.WELCOME_IMAGE_URL, {
-            caption: caption,
-            parse_mode: 'HTML',
-            reply_markup: langKeyboard().reply_markup
-        });
-    } else {
-        await ctx.reply(caption, {
-            parse_mode: 'HTML',
-            reply_markup: langKeyboard().reply_markup
-        });
+        if (config.WELCOME_IMAGE_URL) {
+            await ctx.replyWithPhoto(config.WELCOME_IMAGE_URL, {
+                caption: caption,
+                parse_mode: 'HTML',
+                reply_markup: langKeyboard().reply_markup
+            });
+        } else {
+            await ctx.reply(caption, {
+                parse_mode: 'HTML',
+                reply_markup: langKeyboard().reply_markup
+            });
+        }
+    } catch (err) {
+        console.error('handleStart error:', err);
+        ctx.reply('An error occurred. Please try /start').catch(() => {});
     }
 }
 
 async function handleLanguage(ctx, lang) {
-    const userId = ctx.from.id;
-    await db.updateLanguage(userId, lang);
+    try {
+        const userId = ctx.from.id;
+        await db.updateLanguage(userId, lang);
 
-    const locale = getLocale(lang);
+        const locale = getLocale(lang);
 
-    await ctx.editMessageText(
-        quote(locale.LANG_CHANGED),
-        { parse_mode: 'HTML' }
-    );
+        // Check if original message was a photo or text
+        const msg = ctx.callbackQuery.message;
+        const isPhoto = msg.photo && msg.photo.length > 0;
 
-    setTimeout(() => {
-        ctx.reply(
+        if (isPhoto) {
+            // Edit caption for photo messages
+            await ctx.editMessageCaption(
+                quote(locale.LANG_CHANGED),
+                { parse_mode: 'HTML' }
+            );
+        } else {
+            // Edit text for text messages
+            await ctx.editMessageText(
+                quote(locale.LANG_CHANGED),
+                { parse_mode: 'HTML' }
+            );
+        }
+
+        // Send main menu as new message
+        const premium = await db.isPremium(userId);
+        await ctx.reply(
             quote(locale.MENU_TITLE + '\n\n' + locale.MENU_DESC),
             {
                 parse_mode: 'HTML',
-                reply_markup: require('../utils/keyboards').mainMenuKeyboard(
-                    isOwner(userId),
-                    false
-                ).reply_markup
+                reply_markup: mainMenuKeyboard(isOwner(userId), premium).reply_markup
             }
         );
-    }, 500);
+    } catch (err) {
+        console.error('handleLanguage error:', err);
+        ctx.reply('An error occurred. Please try /start').catch(() => {});
+    }
 }
 
 module.exports = {
