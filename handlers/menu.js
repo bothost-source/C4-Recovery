2abb5f2f3d77259fa27558cb6c6be023a26eb89b
LONER <destinyjob493@gmail.com>
2026-09-03 11:07:35 +0100

    Update menu.js

--- handlers/menu.js
+++ handlers/menu.js
@@ -1,36 +1,66 @@
 const { getLocale, isOwner } = require('../utils/helpers');
 const { quote } = require('../utils/formatters');
-const { mainMenuKeyboard, backKeyboard } = require('../utils/keyboards');
+const { mainMenuKeyboard } = require('../utils/keyboards');
 const db = require('../database/db');
 
 async function handleMenu(ctx) {
-    const userId = ctx.from.id;
-    const user = await db.getUser(userId);
-    const locale = getLocale(user?.language || 'en');
-    const premium = await db.isPremium(userId);
+    try {
+        const userId = ctx.from.id;
+        const user = await db.getUser(userId);
+        const locale = getLocale(user?.language || 'en');
+        const premium = await db.isPremium(userId);
 
-    await ctx.editMessageText(
-        quote(locale.MENU_TITLE + '\n\n' + locale.MENU_DESC),
-        {
-            parse_mode: 'HTML',
-            reply_markup: mainMenuKeyboard(isOwner(userId), premium).reply_markup
+        const msg = ctx.callbackQuery.message;
+        const isPhoto = msg.photo && msg.photo.length > 0;
+
+        const text = quote(locale.MENU_TITLE + '\n\n' + locale.MENU_DESC);
+        const keyboard = mainMenuKeyboard(isOwner(userId), premium).reply_markup;
+
+        if (isPhoto) {
+            await ctx.editMessageCaption(text, {
+                parse_mode: 'HTML',
+                reply_markup: keyboard
+            });
+        } else {
+            await ctx.editMessageText(text, {
+                parse_mode: 'HTML',
+                reply_markup: keyboard
+            });
         }
-    );
+    } catch (err) {
+        console.error('handleMenu error:', err);
+        ctx.reply('An error occurred. Please try /start').catch(() => {});
+    }
 }
 
 async function handleBack(ctx) {
-    const userId = ctx.from.id;
-    const user = await db.getUser(userId);
-    const locale = getLocale(user?.language || 'en');
-    const premium = await db.isPremium(userId);
+    try {
+        const userId = ctx.from.id;
+        const user = await db.getUser(userId);
+        const locale = getLocale(user?.language || 'en');
+        const premium = await db.isPremium(userId);
 
-    await ctx.editMessageText(
-        quote(locale.MENU_TITLE + '\n\n' + locale.MENU_DESC),
-        {
-            parse_mode: 'HTML',
-            reply_markup: mainMenuKeyboard(isOwner(userId), premium).reply_markup
+        const msg = ctx.callbackQuery.message;
+        const isPhoto = msg.photo && msg.photo.length > 0;
+
+        const text = quote(locale.MENU_TITLE + '\n\n' + locale.MENU_DESC);
+        const keyboard = mainMenuKeyboard(isOwner(userId), premium).reply_markup;
+
+        if (isPhoto) {
+            await ctx.editMessageCaption(text, {
+                parse_mode: 'HTML',
+                reply_markup: keyboard
+            });
+        } else {
+            await ctx.editMessageText(text, {
+                parse_mode: 'HTML',
+                reply_markup: keyboard
+            });
         }
-    );
+    } catch (err) {
+        console.error('handleBack error:', err);
+        ctx.reply('An error occurred. Please try /start').catch(() => {});
+    }
 }
 
 module.exports = { handleMenu, handleBack };
